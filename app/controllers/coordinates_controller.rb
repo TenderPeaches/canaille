e9e1fe5e2ba686c984afcc05beaf713e374ec12a
Poubelle <tendrepeches@protonmail.com>
2024-03-26 11:42:56 -0400

    city choice cleanup

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -2,22 +2,22 @@ class CoordinatesController < ApplicationController
     before_action :set_coordinate, only: %i[ show edit update destroy ]
     before_action :set_or_new_coordinate, only: %i[ use_new_city use_existing_city ]
 
-    def show 
+    def show
 
-    end 
+    end
 
-    def index 
+    def index
         @coordinates = Coordinate.all
-    end 
+    end
 
     def new
         @coordinate = Coordinate.new
     end
-    
+
     def create
         @coordinate = Coordinate.new(coordinate_params)
 
-        if (params[:new_city]) 
+        if (params[:new_city])
             respond_to do |format|
                 format.html { render :new }
             end
@@ -53,51 +53,8 @@ class CoordinatesController < ApplicationController
         end
     end
 
-    def use_new_city
-        @coordinate.city = City.new 
-
-        puts params.inspect
-
-        respond_to do |format|
-            # service request => use service-request-specific coordinate
-            if (params[:source] == "service_request_coordinate_attributes_use_new_city")
-                set_or_new_service_request
-                format.turbo_stream { render "service_requests/use_new_city" }
-            # service request => use client's coordinate
-            elsif (params[:source] == "service_request_client_attributes_coordinate_attributes_use_new_city")
-                set_or_new_service_request
-                format.turbo_stream { render "clients/service_requests/use_new_city" }
-            # client portal => set/edit coordinate
-            elsif (params[:source] == "client_coordinate_attributes_use_new_city")
-                @client = current_user.client
-                format.turbo_stream { render "clients/portal/use_new_city" }
-            # service provider => set/edit coordiante
-            elsif (params[:source == "service_provider_coordinate_attributes_use_new_city"])
-                #todo
-            end
-        end
-    end
-
-    def use_existing_city 
-        respond_to do |format|
-            # service request => use service-request-specific coordinate
-            if (params[:source] == "service_request_coordinate_attributes_use_new_city")
-                set_or_new_service_request
-                format.turbo_stream { render "service_requests/use_existing_city" }
-            # service request => use client's coordinate
-            elsif (params[:source] == "service_request_client_attributes_coordinate_attributes_use_new_city")
-                set_or_new_service_request
-                format.turbo_stream { render "clients/service_requests/use_existing_city" }
-            # client portal => set/edit coordinate
-            elsif (params[:source] == "client_coordinate_attributes_use_new_city")
-                @client = current_user.client
-                format.turbo_stream { render "clients/portal/use_existing_city" }
-            end
-        end
-    end
-
-    private 
-    def set_coordinate 
+    private
+    def set_coordinate
         @coordinate = Coordinate.find_by_id(params[:id])
     end
 
@@ -106,19 +63,19 @@ class CoordinatesController < ApplicationController
             @coordinate = Coordinate.new
         end
     end
-    
+
     def set_or_new_service_request
         unless set_service_request
             @service_request = ServiceRequest.new
         end
     end
 
-    def set_or_new_city 
+    def set_or_new_city
         @service_request.coordinate ||= Coordinate.new
         @service_request.coordinate.city ||= City.new
     end
 
-    def set_service_request 
+    def set_service_request
         @service_request = ServiceRequest.find_by_id(params[:id])
     end
 
